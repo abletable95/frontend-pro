9d134bb0d68392bcaee09c73921081aa163b03f0
abletable69 <romis200786@gmail.com>
2021-08-20 14:52:24 +0300

    1

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // 1
 
-let max = (x, y) => {
+/* let max = (x, y) => {
     if (x > y) {
         return x;
     } else {
@@ -9,5 +9,17 @@ let max = (x, y) => {
 }
 
 console.log(max(0, -1));
-
+ */
 // 2
+
+let min = (...numbers) => {
+    let minNumber = numbers[0];
+    for (let i of numbers) {
+        if (i < minNumber) minNumber = i;
+    }
+    return minNumber;
+}
+
+console.log(min(0, -1, 100, 500, 1000))
+
+//3
